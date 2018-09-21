1e55641247216b1b3ca92d6436890f22fbceeea6
mazinator <daniel.maz99@gmail.com>
2018-09-21 14:15:23 +0200

    Mazanek

---
+++ Jetpack/app/src/main/java/kotl/nvs/htl/at/jetpacktutorial/ui/main/MainFragment.kt
@@ -0,0 +1,44 @@
+package kotl.nvs.htl.at.jetpacktutorial.ui.main
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import kotl.nvs.htl.at.jetpacktutorial.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button)?.setOnClickListener{
+            Navigation.findNavController(it).navigate(R.id.action_mainFragment_to_firstFragment)
+        }
+
+        view.findViewById<Button>(R.id.button2)?.setOnClickListener{
+            Navigation.findNavController(it).navigate(R.id.action_mainFragment_to_secondFragment)
+        }
+    }
+
+}
---
+++ Jetpack/app/src/main/java/kotl/nvs/htl/at/jetpacktutorial/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package kotl.nvs.htl.at.jetpacktutorial.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
